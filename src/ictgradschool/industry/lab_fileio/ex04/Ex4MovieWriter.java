7c86f61ae1c7f3ca2dadd4cece93a514ef7e73b1
ycui124 <ycui124@aucklanduni.ac.nz>
2018-08-07 17:18:54 +1200

    ex04

--- src/ictgradschool/industry/lab_fileio/ex04/Ex4MovieWriter.java
+++ src/ictgradschool/industry/lab_fileio/ex04/Ex4MovieWriter.java
@@ -3,6 +3,10 @@ package ictgradschool.industry.lab_fileio.ex04;
 import ictgradschool.industry.lab_fileio.ex03.Movie;
 import ictgradschool.industry.lab_fileio.ex03.MovieWriter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  * Created by anhyd on 20/03/2017.
  */
@@ -12,7 +16,14 @@ public class Ex4MovieWriter extends MovieWriter {
     protected void saveMovies(String fileName, Movie[] films) {
 
         // TODO Implement this with a PrintWriter
-
+        try (PrintWriter pw=new PrintWriter(new File(fileName))) {
+            for (int i = 0; i < films.length; i++) {
+                pw.write(films[i].getName()+","+films[i].getYear()+","+films[i].getLengthInMinutes()
+                        +","+films[i].getDirector()+"\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
